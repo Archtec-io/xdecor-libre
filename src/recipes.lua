499ebd86e5c1cbe826668274109993d3ef694b72
Wuzzy <Wuzzy@disroot.org>
2023-06-29 23:43:42 +0200

    Change packed ice recipe to end Ethereal collision

--- src/recipes.lua
+++ src/recipes.lua
@@ -168,8 +168,9 @@ minetest.register_craft({
 minetest.register_craft({
 	output = "xdecor:packed_ice",
 	recipe = {
-		{"default:ice", "default:ice"},
-		{"default:ice", "default:ice"}
+		{"", "default:ice", ""},
+		{"default:ice", "", "default:ice"},
+		{"", "default:ice", ""},
 	}
 })
 
