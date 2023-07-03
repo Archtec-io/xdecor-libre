99fcea1af970de016753396908a074b0ed5f7039
Wuzzy <Wuzzy@disroot.org>
2023-07-03 16:38:16 +0200

    Remove flora group from ivy
    flora is used for the flower spread ABM

--- src/nodes.lua
+++ src/nodes.lua
@@ -439,7 +439,7 @@ xdecor.register("ivy", {
 	drawtype = "signlike",
 	walkable = false,
 	climbable = true,
-	groups = {snappy = 3, flora = 1, attached_node = 1, plant = 1, flammable = 3},
+	groups = {snappy = 3, attached_node = 1, plant = 1, flammable = 3},
 	paramtype2 = "wallmounted",
 	selection_box = {type="wallmounted"},
 	tiles = {"xdecor_ivy.png"},
