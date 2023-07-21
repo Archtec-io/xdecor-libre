2e8b95dffedbbf4e67a73b669105590fb3828d5e
Wuzzy <Wuzzy@disroot.org>
2023-07-21 16:12:35 +0200

    Add bowl of soup into Creative Inventory

--- src/cooking.lua
+++ src/cooking.lua
@@ -403,7 +403,7 @@ minetest.register_craftitem("xdecor:bowl_soup", {
 	description = S("Bowl of soup"),
 	inventory_image = "xdecor_bowl_soup.png",
 	wield_image = "xdecor_bowl_soup.png",
-	groups = {not_in_creative_inventory=1},
+	groups = {},
 	stack_max = 1,
 	on_use = minetest.item_eat(30, "xdecor:bowl")
 })
