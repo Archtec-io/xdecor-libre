62ef5b8c83911ff74b732706bcff5e3967b665e7
Wuzzy <Wuzzy@disroot.org>
2024-12-26 16:06:56 +0100

    Switch to using node timers for mechanisms

--- src/mechanisms.lua
+++ src/mechanisms.lua
@@ -15,63 +15,90 @@ local PRESSURE_PLATE_AREA_MAX = {x = 2, y = 0, z = 2}
 local LEVER_AREA_MIN = {x = -2, y = -1, z = -2}
 local LEVER_AREA_MAX = {x = 2, y = 1, z = 2}
 
-local function door_toggle(pos_actuator, pos_door, player)
+local PRESSURE_PLATE_PLAYER_RADIUS = 0.8
+
+
+
+local function door_open(pos_door, player)
+	local door = doors.get(pos_door)
+	if not door then
+		return
+	end
+	door:open(player)
+end
+
+local function door_close(pos_door, player)
+	local door = doors.get(pos_door)
+	if not door then
+		return
+	end
+	door:close(player)
+end
+
+-- Returns true if the door node at pos is currently next to any
+-- active actuator node (lever, pressure plate)
+local function door_is_actuatored(pos_door)
+	local minp = vector.add(LEVER_AREA_MIN, pos_door)
+	local maxp = vector.add(LEVER_AREA_MAX, pos_door)
+	local levers = minetest.find_nodes_in_area(minp, maxp, "group:lever")
+	for l=1, #levers do
+		local lnode = minetest.get_node(levers[l])
+		if minetest.get_item_group(lnode.name, "xdecor_actuator") == 2 then
+			return true
+		end
+	end
+	minp = vector.add(PRESSURE_PLATE_AREA_MIN, pos_door)
+	maxp = vector.add(PRESSURE_PLATE_AREA_MAX, pos_door)
+	local pressure_plates = minetest.find_nodes_in_area(minp, maxp, "group:pressure_plate")
+	for p=1, #pressure_plates do
+		local pnode = minetest.get_node(pressure_plates[p])
+		if minetest.get_item_group(pnode.name, "xdecor_actuator") == 2 then
+			return true
+		end
+	end
+	return false
+end
+
+local function actuator_timeout(pos_actuator, actuator_area_min, actuator_area_max)
+	local actuator = minetest.get_node(pos_actuator)
+	-- Turn off actuator
+	if minetest.get_item_group(actuator.name, "xdecor_actuator") == 2 then
+		local def = minetest.registered_nodes[actuator.name]
+		if def._xdecor_actuator_off then
+			minetest.set_node(pos_actuator, { name = def._xdecor_actuator_off, param2 = actuator.param2 })
+		end
+	end
+
+	-- Close neighboring doors that are no longer next to any active actuator
+	local minp = vector.add(actuator_area_min, pos_actuator)
+	local maxp = vector.add(actuator_area_max, pos_actuator)
+	local doors = minetest.find_nodes_in_area(minp, maxp, "group:door")
+	for d=1, #doors do
+		if not door_is_actuatored(doors[d]) then
+			door_close(doors[d])
+		end
+	end
+end
+
+local function actuator_activate(pos_actuator, actuator_area_min, actuator_area_max, player)
 	local player_name = player:get_player_name()
 	local actuator = minetest.get_node(pos_actuator)
-	local door = doors.get(pos_door)
-	if not door then return end
-
-	if minetest.get_item_group(actuator.name, "xdecor_actuator") == 1 then
+	local ga = minetest.get_item_group(actuator.name, "xdecor_actuator")
+	if ga == 2 then
+		-- No-op if actuator is already active
+		return
+	elseif ga == 1 then
 		local def = minetest.registered_nodes[actuator.name]
 		if def._xdecor_actuator_on then
 			minetest.set_node(pos_actuator, { name = def._xdecor_actuator_on, param2 = actuator.param2 })
 		end
 	end
-	door:open(player)
-
-	minetest.after(DISABLE_ACTUATOR_AFTER, function()
-		local actuator_new = minetest.get_node(pos_actuator)
-		if minetest.get_item_group(actuator_new.name, "xdecor_actuator") == 2 then
-			local def = minetest.registered_nodes[actuator_new.name]
-			if def._xdecor_actuator_off then
-				minetest.set_node(pos_actuator, { name = def._xdecor_actuator_off, param2 = actuator_new.param2 })
-			end
-		end
-
-		-- Check if *all* actuator nodes around the door are inactive.
-		-- Only then will the door close. If there's still an active actuator,
-		-- the door stays open.
-		local active_actuator_in_area = false
-		local minp = vector.add(LEVER_AREA_MIN, pos_door)
-		local maxp = vector.add(LEVER_AREA_MAX, pos_door)
-		local levers = minetest.find_nodes_in_area(minp, maxp, "group:lever")
-		for l=1, #levers do
-			local lnode = minetest.get_node(levers[l])
-			if minetest.get_item_group(lnode.name, "xdecor_actuator") == 2 then
-				active_actuator_in_area = true
-				break
-			end
-		end
-		if not active_actuator_in_area then
-			minp = vector.add(PRESSURE_PLATE_AREA_MIN, pos_door)
-			maxp = vector.add(PRESSURE_PLATE_AREA_MAX, pos_door)
-			local pressure_plates = minetest.find_nodes_in_area(minp, maxp, "group:pressure_plate")
-			for p=1, #pressure_plates do
-				local pnode = minetest.get_node(pressure_plates[p])
-				if minetest.get_item_group(pnode.name, "xdecor_actuator") == 2 then
-					active_actuator_in_area = true
-					break
-				end
-			end
-		end
-
-		if not active_actuator_in_area then
-			-- Re-get player object (or nil) because 'player' could
-			-- be an invalid object at this time (player left)
-			local player_new = minetest.get_player_by_name(player_name)
-			door:close(minetest.get_player_by_name(player_name))
-		end
-	end)
+	local minp = vector.add(actuator_area_min, pos_actuator)
+	local maxp = vector.add(actuator_area_max, pos_actuator)
+	local doors = minetest.find_nodes_in_area(minp, maxp, "group:door")
+	for i = 1, #doors do
+		door_open(doors[i], player)
+	end
 end
 
 function plate.construct(pos)
@@ -79,24 +106,42 @@ function plate.construct(pos)
 	timer:start(0.1)
 end
 
-function plate.timer(pos)
-	local objs = minetest.get_objects_inside_radius(pos, 0.8)
-	if not next(objs) or not doors.get then return true end
-
-	local minp = vector.add(PRESSURE_PLATE_AREA_MIN, pos)
-	local maxp = vector.add(PRESSURE_PLATE_AREA_MAX, pos)
-	local doors = minetest.find_nodes_in_area(minp, maxp, "group:door")
-
+function plate.has_player_standing_on(pos)
+	local objs = minetest.get_objects_inside_radius(pos, PRESSURE_PLATE_PLAYER_RADIUS)
 	for _, player in pairs(objs) do
 		if player:is_player() then
-			for i = 1, #doors do
-				door_toggle(pos, doors[i], player)
-			end
-			break
+			return true, player
 		end
+	end
+	return false
+end
+
+function plate.timer(pos)
+	if not doors.get then
+		return true
+	end
+	local ok, player = plate.has_player_standing_on(pos)
+	if ok then
+		actuator_activate(pos, PRESSURE_PLATE_AREA_MIN, PRESSURE_PLATE_AREA_MAX, player)
 	end
 
 	return true
+end
+
+function plate.construct_on(pos)
+	local timer = minetest.get_node_timer(pos)
+	timer:start(DISABLE_ACTUATOR_AFTER)
+end
+
+function plate.timer_on(pos)
+	if plate.has_player_standing_on(pos) then
+		-- If player is still standing on active pressure plate, restart timer
+		local timer = minetest.get_node_timer(pos)
+		timer:start(DISABLE_ACTUATOR_AFTER)
+		return
+	end
+
+	actuator_timeout(pos, PRESSURE_PLATE_AREA_MIN, PRESSURE_PLATE_AREA_MAX)
 end
 
 function plate.register(material, desc, def)
@@ -140,6 +185,8 @@ function plate.register(material, desc, def)
 		drop = "xdecor:pressure_" .. material .. "_off",
 		sunlight_propagates = true,
 		on_rotate = screwdriver.rotate_simple,
+		on_construct = plate.construct_on,
+		on_timer = plate.timer_on,
 		_xdecor_actuator_off = "xdecor:pressure_"..material.."_off",
 		_xdecor_actuator_on = "xdecor:pressure_"..material.."_on",
 	})
@@ -172,15 +219,10 @@ xdecor.register("lever_off", {
 	on_rotate = screwdriver.rotate_simple,
 
 	on_rightclick = function(pos, node, clicker, itemstack)
-		if not doors.get then return itemstack end
-		local minp = vector.add(LEVER_AREA_MIN, pos)
-		local maxp = vector.add(LEVER_AREA_MAX, pos)
-		local doors = minetest.find_nodes_in_area(minp, maxp, "group:door")
-
-		for i = 1, #doors do
-			door_toggle(pos, doors[i], clicker)
+		if not doors.get then
+			return itemstack
 		end
-
+		actuator_activate(pos, LEVER_AREA_MIN, LEVER_AREA_MAX, clicker)
 		return itemstack
 	end,
 	_xdecor_itemframe_offset = -3.5,
@@ -203,6 +245,14 @@ xdecor.register("lever_on", {
 		-- for consistent behavior with the lever in "off" state.
 		-- The player may still place nodes using [Sneak].
 		return itemstack
+	end,
+	on_construct = function(pos)
+		local timer = minetest.get_node_timer(pos)
+		timer:start(DISABLE_ACTUATOR_AFTER)
+	end,
+	on_timer = function(pos)
+		local node = minetest.get_node(pos)
+		actuator_timeout(pos, LEVER_AREA_MIN, LEVER_AREA_MAX)
 	end,
 	drop = "xdecor:lever_off",
 	_xdecor_itemframe_offset = -3.5,
